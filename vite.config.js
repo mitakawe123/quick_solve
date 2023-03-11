1807cd802478935141d5836a451317aa8a725337
Dimitar Kyuchukov <70656129+mitakawe123@users.noreply.github.com>
2023-03-11 20:56:25 +0200

    working on configuration

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    Module: "globalThis.Module",
+  build: {
+    lib: {
+      entry: 'src/main.jsx',
+      name: 'MyLibrary',
+      fileName: 'my-library',
+    },
+    rollupOptions: {
+      output: {
+        format: 'es',
+      },
+    },
   },
 });
